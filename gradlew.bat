7ddc4149cde4cc65f56fd73d5b06f8e7ae6a542b
Therisker1 <73764156+Therisker1@users.noreply.github.com>
2025-06-04 19:26:49 +0300

    Add files via upload

--- gradlew.bat
+++ gradlew.bat
@@ -13,9 +13,7 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-rem Setup JAVA_HOME if not set
 if not defined JAVA_HOME (
-  rem Try to get JAVA_HOME from registry
   for /f "tokens=2*" %%i in ('reg query "HKLM\SOFTWARE\JavaSoft\Java Development Kit" /v CurrentVersion 2^>nul') do set CURRENT_JDK_VERSION=%%j
   if defined CURRENT_JDK_VERSION (
     for /f "tokens=2*" %%i in ('reg query "HKLM\SOFTWARE\JavaSoft\Java Development Kit\%CURRENT_JDK_VERSION%" /v JavaHome 2^>nul') do set JAVA_HOME=%%j
@@ -29,13 +27,8 @@ if not defined JAVA_HOME (
 )
 
 set DEFAULT_JVM_OPTS=
-
-rem Setup GRADLE_OPTS and JAVA_OPTS if needed
 set JVM_OPTS=%DEFAULT_JVM_OPTS% %GRADLE_OPTS% %JAVA_OPTS%
-
 set CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
 
-rem Execute Gradle Wrapper main class
 "%JAVA_HOME%\bin\java" %JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
-
 endlocal
